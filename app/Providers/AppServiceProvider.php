27b357057e0bd5b870aa1079ab7e47cd5c1c8b78
Fairus In'am Pratama <fairusinam08@gmail.com>
2026-05-08 14:50:53 +0700

    Make HTTPS forcing configurable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Vite::prefetch(concurrency: 3);
         
-        if ($this->app->environment('production')) {
+        if (config('app.force_https')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
